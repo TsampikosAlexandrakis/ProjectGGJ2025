5f86405c9807795f5f6cbd31f91d08f12b163b3b
Niephox <rezutasos@gmail.com>
2025-01-26 14:14:53 +0200

    ORder UI

--- Assets/_Project/_Scripts/Runtime/UI/MainUIHandler.cs
+++ Assets/_Project/_Scripts/Runtime/UI/MainUIHandler.cs
@@ -6,18 +6,38 @@ public class MainUIHandler : MonoBehaviour
 {
    public Image orderTimerBG;
    public TMP_Text recipesText; 
+   public VerticalLayoutGroup recipesLayoutGroup;
+   public GameObject reqUI;
+   public TMP_Text timerText;
    public void Start()
    {
        OrderManager.Instance.OnNewOrder += OnNewOrder;
    }
 
+   private void ClearGroup()
+   {
+       foreach (Transform o in recipesLayoutGroup.transform)
+       {
+           Destroy(o.gameObject);
+       }
+   }
+
+   private void SetReqUI()
+   {
+       
+   }
    private void OnNewOrder(OrderSO obj)
    {
+       ClearGroup();
+       
        recipesText.text = "";
        if(obj.Bubbles.Length <= 0 ) return; 
        foreach (var bubble in obj.Bubbles)
        {
-           recipesText.text += $"{bubble.recipe.RecipeName} x{bubble.amount}";
+           GameObject GO = Instantiate(reqUI, recipesLayoutGroup.transform);
+           GO.GetComponentInChildren<Image>().sprite = bubble.recipe.BubbleIcon;
+           GO.GetComponentInChildren<TMP_Text>().text = $"x{bubble.amount}";
+           //recipesText.text += $"{bubble.recipe.RecipeName} x{bubble.amount}";
        }
    }
    
@@ -27,6 +47,7 @@ public class MainUIHandler : MonoBehaviour
       float orderDuration = OrderManager.Instance.currentOrder.expirationTime;
       float fill = currentTimer / orderDuration;
       orderTimerBG.fillAmount = fill;
+      timerText.text = $"{Mathf.FloorToInt(currentTimer)}";
    }
 
    private void FixedUpdate()
